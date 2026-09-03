6fa086352822e78a458152ae768c8c86b107bc25
Obvious Scraptop <71141838+AlexSchoolOH@users.noreply.github.com>
2026-09-03 16:44:34 -0400

    Matrix libraries

---
+++ dist/extras/matrix3.js
@@ -0,0 +1,101 @@
+DaveShade.matrix3 = class {
+    //Two basic construction methods
+    constructor(xx, xy, xz, yx, yy, yz, zx, zy, zz) {
+        this.xx = xx || 0; this.xy = xy || 0; this.xz = xz || 0;
+        this.yx = yx || 0; this.yy = yy || 0; this.yz = yz || 0;
+        this.zx = zx || 0; this.zy = zy || 0; this.zz = zz || 0;
+    }
+
+    duplicate() {
+        return new DaveShade.matrix3(
+            this.xx, this.xy, this.xz,
+            this.yx, this.yy, this.yz,
+            this.zx, this.zy, this.zz
+        );
+    }
+
+    //Simple math
+    multiply(multiplicator) {
+        const resultor = this.duplicate();
+        if (multiplicator instanceof DaveShade.matrix3) {
+            resultor.xx = this.xx * multiplicator.xx + this.xy * multiplicator.yx + this.xz * multiplicator.zx;
+            resultor.xy = this.xx * multiplicator.xy + this.xy * multiplicator.yy + this.xz * multiplicator.zy;
+            resultor.xz = this.xx * multiplicator.xz + this.xy * multiplicator.yz + this.xz * multiplicator.zz;
+
+            resultor.yx = this.yx * multiplicator.xx + this.yy * multiplicator.yx + this.yz * multiplicator.zx;
+            resultor.yy = this.yx * multiplicator.xy + this.yy * multiplicator.yy + this.yz * multiplicator.zy;
+            resultor.yz = this.yx * multiplicator.xz + this.yy * multiplicator.yz + this.yz * multiplicator.zz;
+
+            resultor.zx = this.zx * multiplicator.xx + this.zy * multiplicator.yx + this.zz * multiplicator.zx;
+            resultor.zy = this.zx * multiplicator.xy + this.zy * multiplicator.yy + this.zz * multiplicator.zy;
+            resultor.zz = this.zx * multiplicator.xz + this.zy * multiplicator.yz + this.zz * multiplicator.zz;
+        }
+
+        return resultor;
+    }
+
+    multiplyVector(x, y, z) {
+        if (typeof z != "number") z = 1;
+
+        return [
+            x * this.xx + y * this.xy + z * this.xz,
+            x * this.yx + y * this.yy + z * this.yz,
+            x * this.zx + y * this.zy + z * this.zz,
+        ];
+    }
+
+    //Simple transformations
+    translate(x, y) {
+        return this.multiply(new DaveShade.matrix3(
+            1, 0, x,
+            0, 1, y,
+            0, 0, 1
+        ));
+    }
+
+    rotate(angle) {
+        return this.multiply(new DaveShade.matrix3(
+            Math.cos(angle), Math.sin(angle), 0,
+            -Math.sin(angle), Math.cos(angle), 0,
+            0, 0, 1
+        ));
+    }
+
+    scale(x, y) {
+        return this.multiply(new DaveShade.matrix3(
+            x, 0, 0,
+            0, y, 0,
+            0, 0, 1
+        ));
+    }
+
+    get array() {
+        return [
+            this.xx, this.xy, this.xz,
+            this.yx, this.yy, this.yz,
+            this.zx, this.zy, this.zz
+        ];
+    }
+
+    get translation() {
+        return [ this.xz, this.yz ];
+    }
+
+    get rotation() { return Math.atan2(this.xy, this.xx); }
+
+    get scalar() {
+        let xx = this.xx; let xy = this.xy;
+        let yx = this.yx; let yy = this.yy;
+
+        return [ Math.sqrt(xx * xx + xy * xy), Math.sqrt(yx * yx + yy * yy) ];
+    }
+}
+
+// Just a simple identity matrix
+DaveShade.matrix3.identity = () => {
+    return new DaveShade.matrix3(
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1
+    );
+}
---
+++ dist/extras/matrix4.js
@@ -0,0 +1,155 @@
+DaveShade.matrix4 = class {
+    //Two basic construction methods
+    constructor(xx, xy, xz, xw, yx, yy, yz, yw, zx, zy, zz, zw, wx, wy, wz, ww) {
+        this.xx = xx || 0; this.xy = xy || 0; this.xz = xz || 0; this.xw = xw || 0;
+        this.yx = yx || 0; this.yy = yy || 0; this.yz = yz || 0; this.yw = yw || 0;
+        this.zx = zx || 0; this.zy = zy || 0; this.zz = zz || 0; this.zw = zw || 0;
+        this.wx = wx || 0; this.wy = zy || 0; this.wz = zz || 0; this.ww = ww || 0;
+    }
+
+    duplicate() {
+        return new DaveShade.matrix4(
+            this.xx, this.xy, this.xz, this.xw,
+            this.yx, this.yy, this.yz, this.yw,
+            this.zx, this.zy, this.zz, this.zw,
+            this.wx, this.wy, this.wz, this.ww
+        );
+    }
+
+    //Simple math
+    multiply(multiplicator) {
+        const resultor = this.duplicate();
+        if (multiplicator instanceof DaveShade.matrix4) {
+            resultor.xx = this.xx * multiplicator.xx + this.xy * multiplicator.yx + this.xz * multiplicator.zx;
+            resultor.xy = this.xx * multiplicator.xy + this.xy * multiplicator.yy + this.xz * multiplicator.zy;
+            resultor.xz = this.xx * multiplicator.xz + this.xy * multiplicator.yz + this.xz * multiplicator.zz;
+            resultor.xz = this.xx * multiplicator.xz + this.xy * multiplicator.yz + this.xz * multiplicator.zz;
+
+            resultor.yx = this.yx * multiplicator.xx + this.yy * multiplicator.yx + this.yz * multiplicator.zx;
+            resultor.yy = this.yx * multiplicator.xy + this.yy * multiplicator.yy + this.yz * multiplicator.zy;
+            resultor.yz = this.yx * multiplicator.xz + this.yy * multiplicator.yz + this.yz * multiplicator.zz;
+
+            resultor.zx = this.zx * multiplicator.xx + this.zy * multiplicator.yx + this.zz * multiplicator.zx;
+            resultor.zy = this.zx * multiplicator.xy + this.zy * multiplicator.yy + this.zz * multiplicator.zy;
+            resultor.zz = this.zx * multiplicator.xz + this.zy * multiplicator.yz + this.zz * multiplicator.zz;
+        }
+
+        return resultor;
+    }
+
+    multiplyVector(x, y, z, w) {
+        if (typeof w != "number") w = 1;
+
+        return [
+            x * this.xx + y * this.xy + z * this.xz + w * this.xw,
+            x * this.yx + y * this.yy + z * this.yz + w * this.yw,
+            x * this.zx + y * this.zy + z * this.zz + w * this.zw,
+            x * this.wx + y * this.wy + z * this.wz + w * this.ww
+        ];
+    }
+
+    //Simple transformations
+    translate(x, y, z) {
+        return this.multiply(new DaveShade.matrix4(
+            1, 0, 0, x,
+            0, 1, 0, y,
+            0, 0, 1, z,
+            0, 0, 0, 1
+        ));
+    }
+
+    scale(x, y, z) {
+        return this.multiply(new DaveShade.matrix4(
+            x, 0, 0, 0,
+            0, y, 0, 0,
+            0, 0, z, 0,
+            0, 0, 0, 1
+        ));
+    }
+
+    //Rotations
+    rotateX(angle) {
+        return this.multiply(new DaveShade.matrix4(
+            1, 0, 0, 0,
+            0, Math.cos(angle), Math.sin(angle), 0,
+            0, -Math.sin(angle), Math.cos(angle), 0,
+            0, 0, 0, 1
+        ));
+    }
+
+    rotateY(angle) {
+        return this.multiply(new DaveShade.matrix4(
+            Math.cos(angle), 0, Math.sin(angle), 0,
+            0, 1, 0, 0,
+            -Math.sin(angle), 0, Math.cos(angle), 0,
+            0, 0, 0, 1
+        ));
+    }
+
+    rotateZ(angle) {
+        return this.multiply(new DaveShade.matrix4(
+            Math.cos(angle), Math.sin(angle), 0, 0,
+            -Math.sin(angle), Math.cos(angle), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ));
+    }
+
+    rotateXYZ(x, y, z) { return this.rotateX(x).rotateY(y).rotateZ(z); }
+    rotateZXY(x, y, z) { return this.rotateZ(z).rotateX(x).rotateY(y); }
+    rotateYZX(x, y, z) { return this.rotateY(y).rotateZ(z).rotateX(x); }
+
+    rotateXZY(x, y, z) { return this.rotateX(x).rotateZ(z).rotateY(y); }
+    rotateYXZ(x, y, z) { return this.rotateY(y).rotateX(x).rotateZ(z); }
+    rotateZYX(x, y, z) { return this.rotateZ(z).rotateY(y).rotateX(x); }
+
+    rotateQuaternion(x, y, z, w) {
+        let x2 = x * 2; let y2 = y * 2; let z2 = z * 2;
+        var xx = x * x2; let xy = x * y2; let xz = x * x2;
+        var yy = y * x2; let yz = y * y2; let zz = z * x2;
+        var wx = w * x2; let wy = w * y2; let wz = w * x2;
+
+        return this.multiply(new DaveShade.matrix4(
+            1 - (yy + zz), xy + wz, xz - wy, 0,
+            xy - wz, 1 - (xx + zz), yz + wx, 0,
+            xz + wy, yz - wx, 1 - (xx + yy), 0,
+            0, 0, 0, 1
+        ));
+    }
+
+    get _UNIFORM_VALUE_() {
+        return [
+            this.xx, this.xy, this.xz,
+            this.yx, this.yy, this.yz,
+            this.zx, this.zy, this.zz
+        ];
+    }
+
+    get translation() {
+        return [ this.xw, this.yw, this.zw ];
+    }
+
+    // TODO
+    get rotation() { return Math.atan2(this.xy, this.xx); }
+
+    get scalar() {
+        let xx = this.xx; let xy = this.xy; let xz = this.xz;
+        let yx = this.yx; let yy = this.yy; let yz = this.yz;
+        let zx = this.zx; let zy = this.zy; let zz = this.zz;
+
+        return [
+            Math.sqrt(xx * xx + xy * xy + xz * xz),
+            Math.sqrt(yx * yx + yy * yy + yz * yz),
+            Math.sqrt(zx * zx + zy * zy + zz * zz),
+        ];
+    }
+}
+
+// Just a simple identity matrix
+DaveShade.matrix4.identity = () => {
+    return new DaveShade.matrix4(
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1
+    );
+}
